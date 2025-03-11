d87fb3582ca5d50bfe6e40ae7305c8b88cc72269
freesky <freesky_zh@hotmail.com>
2025-03-11 11:47:47 +0800

    add Spring Boot example

---
+++ src/main/java/com/example/kafka/service/KafkaProducerService.java
@@ -0,0 +1,19 @@
+package com.example.kafka.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducerService {
+
+    private static final String TOPIC = "test";
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send(TOPIC, message);
+        System.out.println("Produced message: " + message);
+    }
+}
