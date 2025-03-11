1e2de426e3ac42e51206bbf1ff6a0b5b4db6dbbf
freesky <freesky_zh@hotmail.com>
2025-03-11 12:12:20 +0800

    define constant in yaml file

--- src/main/java/com/example/kafka/service/KafkaConsumerService.java
+++ src/main/java/com/example/kafka/service/KafkaConsumerService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaConsumerService {
 
-    @KafkaListener(topics = "test", groupId = "my-group")
+    @KafkaListener(topics = "${kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
     public void consume(String message) {
         System.out.println("Consumed message: " + message);
     }
--- src/main/java/com/example/kafka/service/KafkaProducerService.java
+++ src/main/java/com/example/kafka/service/KafkaProducerService.java
@@ -1,19 +1,21 @@
 package com.example.kafka.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaProducerService {
 
-    private static final String TOPIC = "test";
+    @Value("${kafka.topic.name}")
+    private String topic;
 
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        kafkaTemplate.send(TOPIC, message);
+        kafkaTemplate.send(topic, message);
         System.out.println("Produced message: " + message);
     }
 }
